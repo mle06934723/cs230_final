85e0683a4b6970a057341f453abf04954fd13159
mle06934723 <mleshen@stanford.edu>
2024-11-29 21:18:12 -0800

    Update clean_labels.py

--- clean_labels.py
+++ clean_labels.py
@@ -12,70 +12,9 @@ from typing import Dict
 from tqdm import tqdm
 from typing import Dict
 
-class IntegrityClassificationModel(nn.Module):
-    def __init__(self, encoder, num_classes, hidden_size, low_dim):
-        super(IntegrityClassificationModel, self).__init__()
-        self.encoder = encoder
-        self.classifier = nn.Linear(hidden_size, num_classes)
-        self.dim_reducer = nn.Linear(hidden_size, low_dim)
-
-    def forward(self, inputs: Dict[str, torch.Tensor], feat_classifier=False):
-        out = self.encoder(**inputs)
-        out_ = out.pooler_output
-        if(feat_classifier):
-            return out_
-        outContrast = self.dim_reducer(out_)
-        outPred = self.classifier(out_)
-        return outPred, outContrast
-
 encoder = AutoModel.from_pretrained('xlm-roberta-large')
 
 model = IntegrityClassificationModel(encoder, 2, 1024, 128)
-
-class AnthropicDataset(Dataset):
-    def __init__(self, dataset, tokenizer, num_samples=1000, partition='train'):
-        self.tokenizer = tokenizer
-        self.dataset = dataset
-        mini = dataset[partition].shuffle(seed=42).select(range(num_samples))
-        tokenized = mini.map(self.tokenize_function)
-        examples = []
-        self.num_class = 2
-        for elem in tokenized:
-            chosen = elem['chosen']
-            rejected = elem['rejected']
-            chosen_tokens = elem['chosen_tokens_and_mask']
-            rejected_tokens = elem['rejected_tokens_and_mask']
-            chosen_example = {'input_ids': chosen_tokens['input_ids'], 'attention_mask': chosen_tokens['attention_mask'], 'labels': [1]}
-            rejected_example = {'input_ids': rejected_tokens['input_ids'], 'attention_mask': rejected_tokens['attention_mask'], 'labels': [0]}
-            # Add the new examples to the transformed dataset
-            examples.extend([chosen_example, rejected_example])
-        self.examples = pd.DataFrame(examples)
-        self.exs = []
-        self.targets = []
-        for idx in range(len(self.examples)):
-            labels = self.examples.iloc[idx]['labels']
-            self.targets.append(labels)
-
-    def tokenize_function(self, examples):
-        chosen_tokens_and_mask = self.tokenizer(examples['chosen'], padding="max_length", truncation=True, return_tensors='pt')
-        rejected_tokens_and_mask = self.tokenizer(examples['rejected'], padding="max_length", truncation=True, return_tensors='pt')
-        return {
-            'chosen_tokens_and_mask': chosen_tokens_and_mask,
-            'rejected_tokens_and_mask': rejected_tokens_and_mask,
-        }
-
-    def __len__(self):
-        return len(self.examples)
-
-    def __getitem__(self, idx):
-        data = {
-            "input_ids": torch.squeeze(torch.tensor(self.examples.iloc[idx]['input_ids'])),
-            "attention_mask": torch.squeeze(torch.tensor(self.examples.iloc[idx]['attention_mask'])),
-
-        }
-        labels = torch.tensor(self.examples.iloc[idx]['labels'])
-        return data, labels, idx
-
 
 dataset = load_dataset("Anthropic/hh-rlhf")
 tokenizer = AutoTokenizer.from_pretrained("xlm-roberta-large")
